88480ee99e460bee27eddeefba47099cbeba36fb
paquijanoc <111616862+paquijanoc@users.noreply.github.com>
2023-05-09 12:20:35 -0500

    P1

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,20 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+import csv
+
+# Lee los registros del archivo credit.csv y emite los pares clave-valor
+with open('credit.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)  # Omitir encabezado
+    
+    for row in reader:
+        credit_history = row[2]  # Obtiene el valor de la tercera columna (credit_history)
+        
+        # Dividir la columna credit_history en palabras
+        words = credit_history.split()
+        
+        # Emite cada palabra como clave con valor 1
+        for word in words:
+            print(f'{word}\t1')
--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,3 +1,25 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+current_word = None
+count = 0
+
+# Lee los pares clave-valor del mapper y realiza la reducción
+for line in sys.stdin:
+    line = line.strip()
+    word, value = line.split('\t', 1)
+    
+    if current_word == word:
+        count += int(value)
+    else:
+        if current_word:
+            # Emite el resultado de la palabra anterior
+            print(f'{current_word}\t{count}')
+        current_word = word
+        count = int(value)
+
+# Emite el resultado de la última palabra
+if current_word:
+    print(f'{current_word}\t{count}')
